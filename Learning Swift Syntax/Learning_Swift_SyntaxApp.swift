51c0a7a4c8584f67bd6763a9fd7ed2ee5257e689
eensungkim <eensungkim@gmail.com>
2023-10-18 11:21:57 +0900

    Initial Commit

---
+++ Learning Swift Syntax/Learning_Swift_SyntaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_Swift_SyntaxApp.swift
+//  Learning Swift Syntax
+//
+//  Created by Kim EenSung on 10/18/23.
+//
+
+import SwiftUI
+
+@main
+struct Learning_Swift_SyntaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
